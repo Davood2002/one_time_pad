88c0e5379ad341178ba16d1eed3cade22c8f5688
Davood K.M <77622614+Davood2002@users.noreply.github.com>
2024-03-16 16:19:29 +0330

    Update XOR.py

--- XOR.py
+++ XOR.py
@@ -1,29 +1,32 @@
+''' gets two binary files as arguments, 
+performs XOR operation on them bit by bit (bit-wise),
+and returns the result as a binary file,
+named "out.bin". '''
+
 from sys import argv as arg
 
-# define a function to read binary files
+# a function that reads binary files
 def read_binary_file(file_path) -> bytearray:
     with open(file_path, 'rb') as file:
         array = bytearray(file.read())
         return array
 
-# Perform XOR operation byte by byte
+# a functions that perform XOR operation byte by byte
 def XOR(array1 , array2):
     xor_result = bytearray()
     for byte1 , byte2 in zip(array1 , array2):
         xor_result.append(byte1 ^ byte2)
     return xor_result
-    
+
+# store input files as bytesarray objecs   
 path1 = arg[1]
 path2 = arg[2]
-
 array1 = read_binary_file(path1)
 array2 = read_binary_file(path2)
 
+# calculate the result
 xor_result = XOR(array1 , array2)
 
 # save result to a new file
 with open("out.bin" , "wb+") as output_file:
     output_file.write(xor_result)
-
-len(array2)
-len(XOR(array1 , array2))
